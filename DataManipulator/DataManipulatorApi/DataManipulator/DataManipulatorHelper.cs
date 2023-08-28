8545b080a79947199c06a7074f1c0a5f1bd91634
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-08-28 22:23:26 +0300

    UC#1 Create filter by name/common function

---
+++ DataManipulator/DataManipulatorApi/DataManipulator/DataManipulatorHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace DataManipulatorApi.DataManipulator
+{
+    using DataManipulatorApi.Constants;
+    using DataManipulatorApi.Models;
+
+    public class DataManipulatorHelper
+    {
+        public DataManipulatorHelper(IEnumerable<CountryViewModel> collection)
+        {
+            this.Collection = collection;
+        }
+
+        public IEnumerable<CountryViewModel> Collection { get; set; }
+
+        public void FilterByCommonName(string commonName)
+        {
+            this.Collection = this.Collection
+                .Where(x => x.Name.Common.Contains(commonName, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
