a21263afb8add8e3019785918e41453bf7ead1fe
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-08-28 22:50:34 +0300

    UC#1 Add static to GlobalConstants

--- DataManipulator/DataManipulatorApi/Constants/GlobalConstants.cs
+++ DataManipulator/DataManipulatorApi/Constants/GlobalConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace DataManipulatorApi.Constants
 {
-    public class GlobalConstants
+    public static class GlobalConstants
     {
         public const string COUNTRIES_API_BASE_URL = "https://restcountries.com/v3.1/";
         public const string COUNTRIES_CLIENT_NAME = "Countries";
