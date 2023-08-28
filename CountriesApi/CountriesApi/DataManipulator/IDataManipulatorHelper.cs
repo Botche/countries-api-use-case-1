a88ed1786626d73831b20420c0a4cd38af314144
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-08-28 23:19:47 +0300

    UC#1 Introduce Interface

---
+++ CountriesApi/CountriesApi/DataManipulator/IDataManipulatorHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace CountriesApi.DataManipulator
+{
+    using CountriesApi.Models;
+
+    public interface IDataManipulatorHelper
+    {
+        public IEnumerable<CountryViewModel> Countries { get; set; }
+
+        public void FilterByCommonName(string? commonName);
+
+        public void FilterByPopulation(int? population);
+
+        public void SortByCommonName(string? sort);
+
+        public void LimitTheRecords(int? numberOfRecordst);
+    }
+}
